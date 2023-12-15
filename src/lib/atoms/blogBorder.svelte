609ed40d75a8ef458ba3bcefaabfbe42c290cfd7
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2023-12-15 23:16:36 +0100

    cards - border - desktop versie

--- src/lib/atoms/blogBorder.svelte
+++ src/lib/atoms/blogBorder.svelte
@@ -1,4 +1,32 @@
+<section>
+  <!-- CARDS - BORDER -->
+  <article></article>
+  <article></article>
+  <article></article>
+  <!-- CARDS - BORDER -->
+  <article></article>
+  <article></article>
+  <article></article>
+</section>
+
 <style>
+  section {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 2em;
+    padding: 2em;
+  }
+
+  article {
+    height: 60vh;
+    width: 23vw;
+    border: 4px solid var(--vtDarkBlue);
+    border-radius: 0.5em;
+  }
+
   /* tablet */
   @media (min-width: 31em) and (max-width: 55em) {
   }
