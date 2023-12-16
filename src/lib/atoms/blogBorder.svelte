36816d936e0b175ac6bdf90cd5301a804c03a66b
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2023-12-16 01:13:51 +0100

    responsief bijgewerkt

--- src/lib/atoms/blogBorder.svelte
+++ src/lib/atoms/blogBorder.svelte
@@ -17,7 +17,8 @@
     align-items: center;
     flex-wrap: wrap;
     gap: 2em;
-    padding: 2em;
+    padding: 3em;
+    margin: 3em;
   }
 
   article {
@@ -29,9 +30,31 @@
 
   /* tablet */
   @media (min-width: 31em) and (max-width: 55em) {
+    section {
+      flex-direction: row;
+      justify-content: space-evenly;
+      align-items: center;
+      margin: -1em;
+    }
+
+    article {
+      width: 39vw;
+      height: 65vh;
+    }
   }
 
   /* mobile */
   @media (max-width: 31em) {
+    section {
+      flex-direction: column;
+      justify-content: space-evenly;
+      align-items: center;
+      margin: -1em;
+    }
+
+    article {
+      height: 60vh;
+      width: 50vw;
+    }
   }
 </style>
--- src/routes/artikelen/+page.svelte
+++ src/routes/artikelen/+page.svelte
@@ -1,12 +1,14 @@
 <script>
-import Header from "$lib/molecules/header.svelte";
-import BlogBorder from "$lib/atoms/blogBorder.svelte";
-import BlogButton from "$lib/atoms/blogButton.svelte";
-import BlogHex from "$lib/atoms/blogHex.svelte";
-import Footer from "$lib/molecules/footer.svelte";
+  import Header from "$lib/molecules/header.svelte";
+  import BlogBorder from "$lib/atoms/blogBorder.svelte";
+  import BlogText from "$lib/atoms/blogText.svelte";
+  import BlogButton from "$lib/atoms/blogButton.svelte";
+  import BlogHex from "$lib/atoms/blogHex.svelte";
+  import Footer from "$lib/molecules/footer.svelte";
 </script>
 
 <Header />
+<!-- <BlogText /> -->
 <BlogBorder />
 <BlogButton />
 <BlogHex />
