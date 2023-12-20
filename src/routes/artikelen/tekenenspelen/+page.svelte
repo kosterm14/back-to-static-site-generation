7a61e98edd20895010fd4fbc7acbd969a3a5a92a
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2023-12-20 21:25:59 +0100

    done homepage

---
+++ src/routes/artikelen/tekenenspelen/+page.svelte
@@ -0,0 +1,23 @@
+
+
+<header>
+  <section>
+    <h4 class="line">Case</h4>
+  </section>
+</header>
+
+<style>
+  .line {
+    text-transform: uppercase;
+    background-color: var(--vtLightBlue);
+    color: var(--vtWhite);
+    font-family: var(--vtPrimaryFont);
+    font-size: 0.9rem;
+    padding-left: 7rem;
+    padding-top: 0.2rem;
+    padding-bottom: 0.2rem;
+    margin-top: 0%;
+    display: flex;
+    align-items: center;
+  }
+</style>
